a6b7b976baedb6f20517f2e7b9dac1dac94fb4f5
Tyson.decker@gmail.com <Tyson.decker@gmail.com@8b0e373f-eeae-4230-e970-f57baa1588cf>
2013-04-11 05:11:57 +0000

    Started writing the Depends on Graph. The map is made and nodes are added to it. The nodes are also populated with their children being the nodes they depend on.

---  byu-cs236-project5/236Project5/Graph.cpp
+++  byu-cs236-project5/236Project5/Graph.cpp
@@ -1,13 +1,43 @@
 #include "Graph.h"
+#include <iostream>
 
 
 Graph::Graph(void){}
 Graph::~Graph(void){}
 
 Graph::Graph(std::string n, std::vector<Rule> r){
+
+	std::vector<Rule>::iterator ruleIter;
+	std::map<std::string, Node>::iterator graphIter;
+
+	createGraph(r);
+
+	for ( graphIter = graph.begin(); graphIter != graph.end(); graphIter++) {
+			std::cout << graphIter->first << "\n";
+			std::cout << "\t" << graphIter->second.toString() << "\n";
+	}
+
 }
 
-void Graph::createGraph(){
+void Graph::createGraph(std::vector<Rule> r){
+	std::vector<Rule>::iterator ruleIter;
+
+	for ( ruleIter = r.begin(); ruleIter < r.end(); ruleIter++) {
+
+		if ( graph.find(ruleIter->getHead().getName()) == graph.end() ) {
+			Node temp = Node();
+
+			for(int i = 0; i < ruleIter->getPredicateNames().size(); i++)
+				temp.addChild(ruleIter->getPredicateNames().at(i));	
+
+				graph[ruleIter->getHead().getName()] = temp;
+
+		} else {
+			for(int i = 0; i < ruleIter->getPredicateNames().size(); i++)
+				graph[ruleIter->getHead().getName()].addChild(ruleIter->getPredicateNames().at(i));
+		}
+			
+	}
 }
 
 void Graph::buildDFStree(){
